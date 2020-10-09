8cf1ad4f379f333bd4283a7a24ee4e15cb47e720
LosGroschos <chiller108@googlemail.com>
2020-10-09 14:40:29 +0200

    hui

---
+++ MLAB/parameterariation.m
@@ -0,0 +1,18 @@
+%% Init
+clc;
+clear;
+close all;
+%% Import Data
+d.a1 = importfile('../DATA/Mappe1.xlsx');
+d.a2 = importfile('../DATA/Mappe2.xlsx');
+d.a3 = importfile('../DATA/Mappe3.xlsx');
+d.a4 = importfile('../DATA/Mappe4.xlsx');
+d.a5 = importfile('../DATA/Mappe5.xlsx');
+d.a6 = importfile('../DATA/Mappe6.xlsx');
+
+
+
+%% Plot
+
+
+
